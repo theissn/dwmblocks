7418c916bb2cd59205d0991710c59f9175c4ed60
theissn <theissnielsen@gmail.com>
2022-01-01 23:03:55 +0000

    added blocks

---
+++ blocks.h
@@ -0,0 +1,12 @@
+static const Block blocks[] = {
+	{"", "sb-torrent", 10, 0},
+	{"", "sb-disk", 5, 0},
+	{"", "sb-internet", 5, 4},
+	{"", "sb-vol", 0, 10},
+	{"", "sb-mem", 10, 14},
+	{"", "sb-battery", 5, 3},
+  {"", "sb-clock", 60, 1},
+};
+
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
